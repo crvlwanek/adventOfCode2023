aa5d3e4e8fad7bf3df2094ae9691d9bd534b281b
ChrisVL-W <chris.vanlanenwanek@gmail.com>
2023-12-09 14:05:54 -0600

    Finish day 6

--- day6/solution.py
+++ day6/solution.py
@@ -1,6 +1,21 @@
 with open("input.txt") as file:
     lines = [line.rstrip() for line in file.readlines()]
 
-times, distances = [[string for string in line.split(" ") if string and string.isdigit()] for line in lines]
-print(times)
-print(distances)
+def part1(lines):
+    times, distances = [[int(string) for string in line.split(" ") if string and string.isdigit()] for line in lines]
+
+    total = 1
+    for time, distance in zip(times, distances):
+        race = [i * (time - i) for i in range(time + 1)]
+        total *= sum(True for r in race if r > distance)
+
+    print(total)
+
+def part2BruteForce(lines):
+    time = int("".join(char for char in lines[0] if char.isdigit()))
+    distance = int("".join(char for char in lines[1] if char.isdigit()))
+
+    race = [i * (time - i) for i in range(time + 1)]
+    print(sum(True for r in race if r > distance))
+
+part2BruteForce(lines)
